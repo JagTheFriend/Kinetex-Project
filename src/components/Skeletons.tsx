6aeb45f634eded29c3521386601bef2371dcd592
JagTheFriend <JagTheFriend12@gmail.com>
2025-12-03 19:23:18 +0530

    Display skeleton
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/Skeletons.tsx
+++ src/components/Skeletons.tsx
@@ -1,3 +1,27 @@
+import { Skeleton } from "./ui/skeleton";
+
+const SkeletonCard = () => {
+	return (
+		<article className="flex flex-col overflow-hidden rounded-lg bg-card card-shadow hover:card-shadow-hover border-2 cursor-progress">
+			<div className="relative aspect-16/10 overflow-hidden">
+				<Skeleton className="h-full w-full bg-neutral-300" />
+			</div>
+
+			<div className="flex flex-1 flex-col p-4">
+				<Skeleton className="h-12 w-full bg-neutral-300" />
+			</div>
+		</article>
+	);
+};
+
 export default function Skeletons() {
-	return <></>;
+	return (
+		<div className="flex flex-col gap-8">
+			<div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+				<SkeletonCard />
+				<SkeletonCard />
+				<SkeletonCard />
+			</div>
+		</div>
+	);
 }
